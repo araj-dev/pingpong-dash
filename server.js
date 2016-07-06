09a1fa9a08832fb2dc6ef95c5c748da21a1995dc
HiromiSakurai <sakurai465270@gmail.com>
2016-07-06 16:29:53 +0900

    server

--- server.js
+++ server.js
@@ -21,16 +21,52 @@ var server = http.createServer(router);
 var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
-var messages = [];
-var sockets = [];
+
+
+//--------------//
+var rooms = [];
 
 io.on('connection', function (socket) {
   console.log("ID: "+socket.id.substring(2)+"has connected");
-  
+
   socket.on('event_name',function(data){
     console.log(data);
     socket.emit('send',{aisatsu:"こんにちは"});
   });
+
+
+//-----主催者用のイベント-------//
+  socket.on('makeRoom',function(){
+      var roomname = Math.floor(Math.random()*(9999-1000)+1000);
+      rooms[] = roomname;
+      socket.roomname = roomname;
+      socket.flg = 1 ;
+      console.log(socket);
+  });
+
+  socket.on('choice',function(data){
+    if(!socket.flg){
+      return;
+    }
+    // rooms[socket.roomname].foreach(socket){
+    //   socket.emit('choice',data);
+    io.to(roomname).broadcast()
+    }
+  });
+
+
+
+//----参加者用のイベント-------//
+  socket.on('joinRoom',function(roomname){
+    //roomsの配列にdata.roomnameがあるかどうかチェック
+    if([!rooms[]=data.noomname]){
+      return;
+    }
+      //ある場合、その配列に自分のsocketを追加する。
+      socket.join(roomname);
+      console.log(roomname);
+  });
+
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
